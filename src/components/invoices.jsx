c746fc86beab77c867096082b120f4bf36a5c818
Abdulaziz <abdulazizprogrammer777@gmail.com>
2025-11-13 11:01:10 +0500

    aa

--- src/components/invoices.jsx
+++ src/components/invoices.jsx
@@ -2,10 +2,10 @@ import InvoiceCard from './InvoiceCard'
 
 export default function Invoices({ invoices, loading, error }) {
 	if (loading) {
-		;<h1 className='container mx-auto text-6xl text-slate-500'>Loading...</h1>
+		return<h1 className='container mx-auto text-6xl  text-slate-500'>Loading...</h1>
 	}
 	if (error) {
-		;<h1>Xatolik yuz berdi</h1>
+		return <h1>Xatolik yuz berdi</h1>
 	}
 	return (
 		<div className='flex flex-col gap-4 container mx-auto px-5'>
